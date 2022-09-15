7fe686f99851d7122d807832361affd442343a92
Emmanuel <emmanuel.mollard@epita.fr>
2022-09-15 12:10:31 +0200

    [lab_02] load right dataset

--- lab_02/main.py
+++ lab_02/main.py
@@ -1,6 +1,5 @@
-from datasets import load_dataset, get_dataset_split_names
+from datasets import load_dataset
 
 if __name__ == "__main__":
-    #ds_split_name = get_dataset_split_names("imdb")
-    dataset = load_dataset("rotten_tomatoes", split="train")
+    dataset = load_dataset("imdb")
     print(dataset)
