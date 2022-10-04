c6d1c87d7f966e876793b4c9b02ab581f5a87ffe
Emmanuel <emmanuel.mollard@epita.fr>
2022-10-04 17:53:36 +0200

    [lab_02.ipynb] move lower_and_remove_punctuation to lib/util

---
+++ lib/util.py
@@ -0,0 +1,12 @@
+"""TODO
+"""
+
+from string import punctuation
+
+def lower_and_remove_punctuation(dict_elt : dict) -> dict:
+    '''
+    dict_elt : Lower and remove the punctuation in an element of a dictionary of the dataset, it requires ['text']
+    return the dict after updating the text value
+    '''
+    dict_elt['text'] = dict_elt['text'].lower().translate(str.maketrans('', '', punctuation))
+    return dict_elt
